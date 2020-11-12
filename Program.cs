89a357cd3598464be2fa9530685c8624ceb31672
Bornemissz Zsigmond <bornemissza.zsigmond@ganziskola.hu>
2020-11-12 12:13:01 +0100

    Fájl beolvasás

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace autokStructArray
     {
         static void Main(string[] args)
         {
+            //Fájl beolvasás
+
+            Console.ReadKey();
         }
     }
 }
